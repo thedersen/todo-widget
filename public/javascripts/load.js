b6dfd59463e91a93547f5c6d0d74f0472aa316b9
thedersen <thedersen@gmail.com>
2012-04-15 00:53:30 +0200

    Injects the css before loading the app

--- public/javascripts/load.js
+++ public/javascripts/load.js
@@ -1,6 +1,22 @@
+var injectHtml = function () {
+    var ourScriptTag = $('script[src*="todoapp.js"]').first(),
+    	src = ourScriptTag.attr('src'),
+    	rootUrl = src.substring(0, src.indexOf('widget')),
+    	stylesheetUrl = rootUrl + 'stylesheets/todoapp.css';
+
+    if (typeof document.createStyleSheet !== 'undefined') {
+        document.createStyleSheet(stylesheetUrl);
+    } else {
+        $('<link href="' + stylesheetUrl + '" rel="stylesheet" type="text/css">').prependTo($('head'));
+    }
+};
+
 // Load the application once the DOM is ready, using `jQuery.ready`:
 $(function(){
-  // Finally, we kick things off by creating the **App**.
-  var App = new AppView();
+
+	injectHtml();
+
+  	// Finally, we kick things off by creating the **App**.
+  	var App = new AppView();
 
 });
